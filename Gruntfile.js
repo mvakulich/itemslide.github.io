5ef4b6455c81bb13cfdbaa9f7b2ec5d303162fed
Misha Vakulich <misha@switchapp.com>
2015-07-02 15:01:40 +0300

    prevent link clicks after scroll

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,10 +3,6 @@ module.exports = function (grunt) {
     grunt.initConfig({
         uglify: {
             regular: {
-                options : {
-                  compress : false,
-                  beautify: true
-                },
                 files: {
                     'dist/itemslide.min.js': ['src/requestAnimationFrame.js', 'src/itemslide.js', 'src/slideout.js']
                 }
